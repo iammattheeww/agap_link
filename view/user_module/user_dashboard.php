cc54c928bbf4adc2a55c06c82a5647f95136afda
iammattheeww <bhorbhor2014@gmail.com>
2026-02-08 10:58:01 +0800

    Gin update ko ang directories.

--- view/user_module/user_dashboard.php
+++ view/user_module/user_dashboard.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
-  header("Location: /agap_link/login/index.php");
+    header("Location: /agap_link/view/auth/index.php");
     exit();
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,10 +16,12 @@ if (!isset($_SESSION['user_id'])) {
     <title>User Dashboard - AGAP-Link</title>
     <link rel="stylesheet" href="../agap_link/assets/css/style.css">
 </head>
+
 <body>
     <h1>Welcome, <?= htmlspecialchars($_SESSION['user_name']) ?>!</h1>
     <p>You are now logged in.</p>
-    
-<a href="/agap_link/login/logout.php">Logout</a>
+
+    <a href="/agap_link/view/auth/index.php">Logout</a>
 </body>
+
 </html>
