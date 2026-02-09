4c6129f0e9cc2c12add7e97062c03237090a7ed8
iammattheeww <bhorbhor2014@gmail.com>
2026-02-09 22:27:23 +0800

    blablabla

--- config/agaplinkdb.php
+++ config/agaplinkdb.php
@@ -1,8 +1,20 @@
 <?php
+// FOR HELIOHOST SETUP
+// define('DB_HOST', 'localhost');  // or morty.heliohost.org if localhost doesn't work
+// define('DB_USER', 'agaplink_admin');  // Replace with your actual username
+// define('DB_PASS', 'JayramGarcia123');
+// define('DB_NAME', 'agaplink_db');  // Replace with your actual database name
+
+// OR USING THE ORIGINAL FORMAT
 $host = "localhost";
 $dbname = "agap_link";
-$username = "root";
-$password = "";
+$username ="root";
+$password="";
+
+// $host = "morty.heliohost.org";
+// $dbname = "agaplink_db";
+// $username = "agaplink_admin";
+// $password = "JayramGarcia123";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
